6a21587aed977b0ae7240125735b5be41db511de
boschma2702 <reneboschma@hotmail.com>
2017-03-27 17:16:14 +0200

    account stubs added

---
+++ src/main/java/com/bank/command/customer/CustomerDeleteCommand.java
@@ -0,0 +1,14 @@
+package com.bank.command.customer;
+
+public class CustomerDeleteCommand {
+
+    private int customerId;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+}
