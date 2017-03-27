163c0cac24cc7f63d8c50850cc2eb989f4ee3359
boschma2702 <reneboschma@hotmail.com>
2017-03-27 17:16:14 +0200

    account stubs added

---
+++ src/main/java/com/bank/controller/AccountController.java
@@ -0,0 +1,74 @@
+package com.bank.controller;
+
+import com.bank.command.account.AccountCreateCommand;
+import com.bank.command.account.AccountDeleteCommand;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+@RequestMapping("/rest/account")
+public class AccountController {
+
+
+    @ApiOperation(value = "Used to create a new account. ",
+        notes = "Used to create a new account.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Account successfully added")
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.PUT)
+    public void createAccount(HttpSession session, AccountCreateCommand accountCreateCommand){
+
+    }
+
+    @ApiOperation(value = "Used to delete an account",
+        notes = "This does not remove the account from the database. The account will be made inactive. This keeps the " +
+                "foreign keys intact.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Account successfully deleted")
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void deleteAccount(HttpSession session, @RequestBody AccountDeleteCommand accountDeleteCommand){
+
+    }
+
+    @ApiOperation(value = "Used to get the amount on the account",
+            notes = "Gets the amount currently stored on the given account. ")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Account successfully deleted")
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/amount/{acountId}", method = RequestMethod.GET)
+    public void getAmount(HttpSession session, @PathVariable int acountId){
+
+    }
+
+    @ApiOperation(value = "Used to get all the users on this account",
+            notes = "Returns a list of all the users currently assigned to this account.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Account successfully deleted")
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/customers/{acountId}", method = RequestMethod.GET)
+    public void getCustomers(){
+
+    }
+
+    @ApiOperation(value = "Used to get all the cards on this account",
+            notes = "Returns a list of all the cards currently assigned to this account.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Account successfully deleted")
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/cards/{acountId}", method = RequestMethod.GET)
+    public void getCards(){
+
+    }
+
+
+}
