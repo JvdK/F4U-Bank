c01c3502f64da0e923d686cdaaa408b447e9f06e
boschma2702 <reneboschma@hotmail.com>
2017-03-27 17:16:14 +0200

    account stubs added

---
+++ src/main/java/com/bank/command/account/AccountCreateCommand.java
@@ -0,0 +1,14 @@
+package com.bank.command.account;
+
+public class AccountCreateCommand {
+
+    int customer_id;
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+}
---
+++ src/main/java/com/bank/command/account/AccountDeleteCommand.java
@@ -0,0 +1,14 @@
+package com.bank.command.account;
+
+public class AccountDeleteCommand {
+
+    private int accountId;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+}
