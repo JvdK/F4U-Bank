ea5f85a60caaac8dddbd57cb2c4deffbf5d922b1
boschma2702 <reneboschma@hotmail.com>
2017-04-08 11:41:44 +0200

    added customer update functionality

---
+++ src/main/java/com/bank/command/customer/CustomerUpdateCommand.java
@@ -0,0 +1,104 @@
+package com.bank.command.customer;
+
+public class CustomerUpdateCommand {
+
+    private int customerId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String initials;
+
+    private String address;
+
+    private String phone;
+
+    private String postalCode;
+
+    private String city;
+
+    private String country;
+
+    private String email;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
