759ade98ee2a8090722d754a34faad30bc6d8984
boschma2702 <reneboschma@hotmail.com>
2017-04-23 14:28:32 +0200

    added getAmount of account

--- src/main/java/com/bank/repository/account/AccountRepository.java
+++ src/main/java/com/bank/repository/account/AccountRepository.java
@@ -1,10 +1,13 @@
 package com.bank.repository.account;
 
 import com.bank.bean.customer.AccountBean;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends CrudRepository<AccountBean, Integer> {
 
+    @Query("select a.amount from AccountBean a where a.accountId = ?1")
+    double getAmountOfAccount(int accountId);
 }
