1982e4f937c6f441a4699da573c3cbe54d5fa9a5
boschma2702 <reneboschma@hotmail.com>
2017-04-23 16:07:18 +0200

    added adding customer to account

--- src/main/java/com/bank/service/account/AccountCustomerService.java
+++ src/main/java/com/bank/service/account/AccountCustomerService.java
@@ -1,5 +1,6 @@
 package com.bank.service.account;
 
+import com.bank.bean.customer.CustomerAccount;
 import com.bank.projection.account.AccountCustomerDetailsProjection;
 import com.bank.repository.account.AccountCustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,11 @@ public class AccountCustomerService {
     AccountCustomerRepository accountCustomerRepository;
 
     public List<AccountCustomerDetailsProjection> getCustomersOfAccount(int accountId){
-        return accountCustomerRepository.findCustomerAccountIdsByAccountId(accountId);
+        return accountCustomerRepository.findCustomerAccountsByAccountId(accountId);
+    }
+
+    public void addCustomerAccount(CustomerAccount customerAccount){
+        System.out.println(customerAccount);
+        accountCustomerRepository.save(customerAccount);
     }
 }
