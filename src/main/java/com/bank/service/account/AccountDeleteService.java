8f2cfb2bf1736798d04f69fed3fd3e30a3ff83d5
boschma2702 <reneboschma@hotmail.com>
2017-04-26 09:00:44 +0200

    added inactivation of accounts

---
+++ src/main/java/com/bank/service/account/AccountDeleteService.java
@@ -0,0 +1,25 @@
+package com.bank.service.account;
+
+import com.bank.bean.customer.AccountBean;
+import com.bank.command.account.AccountDeleteCommand;
+import com.bank.exception.NotFoundException;
+import com.bank.repository.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDeleteService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public void deleteAccount(AccountDeleteCommand command) throws NotFoundException {
+        AccountBean bean = accountRepository.findOne(command.getAccountId());
+        if(bean == null){
+            throw new NotFoundException();
+        }
+        bean.setActive(false);
+        accountRepository.save(bean);
+    }
+
+}
