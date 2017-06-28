e0a5953767695cad42bc98cfffc041a957effae6
JvdK <j.vanderkaaij@student.utwente.nl>
2017-06-28 15:58:29 +0200

    Add JsonRpc dependency and configuration

---
+++ src/main/java/com/bank/configuration/JsonRpcConfiguration.java
@@ -0,0 +1,16 @@
+package com.bank.configuration;
+
+import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImplExporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonRpcConfiguration {
+    @Bean
+    public static AutoJsonRpcServiceImplExporter autoJsonRpcServiceImplExporter() {
+        AutoJsonRpcServiceImplExporter exp = new AutoJsonRpcServiceImplExporter();
+        exp.setAllowLessParams(false);
+
+        return exp;
+    }
+}
